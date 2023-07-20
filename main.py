1e2664d0aec5099514862e72c6198c4cdfdef605
DmitriYam <dmitriyam.rex1738@gmail.com>
2023-07-20 11:23:49 +0100

    min working version

--- main.py
+++ main.py
@@ -4,8 +4,13 @@ from tkinter import messagebox
 from PIL import ImageTk, Image
 import os
 import pygame
+import RPi.GPIO as GPIO
 
 STATE="mainMenu"
+BUTTON1 = 0
+BUTTON2=False
+BUTTON3=False
+
 #init PREV_STATE to null 
 PREV_STATE="null"
 root = Tk()
@@ -39,12 +44,35 @@ def playAudio(voicefile):
     pygame.mixer.music.play()
     while pygame.mixer.music.get_busy() == True:
         continue
+#interrupt service routines
+def isr1(channel):
+    global BUTTON1
+    BUTTON1 = 1
+    print("Button 1")
+def isr2(channel):
+    global BUTTON2
+    BUTTON2 = True
+    print("Button 2")
+def isr3(channel):
+    global BUTTON3
+    BUTTON3 = True
+    print("Button 3")
 
+#GPIO
+def initGPIO():
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(17, GPIO.FALLING,callback=isr1, bouncetime=200)
+    GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(26, GPIO.FALLING,callback=isr2, bouncetime=200)
+    GPIO.setup(22, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+    GPIO.add_event_detect(22, GPIO.FALLING,callback=isr3, bouncetime=200)
+
+initGPIO()
 initAudio()
     
 while True:
     #check for change in state
-
     if PREV_STATE != STATE:
         if(STATE=="mainMenu"):
             mainMenu()
@@ -58,12 +86,16 @@ while True:
         root.update()
         playAudio(audiofile)
         PREV_STATE = STATE
-    
-    #poll for io changes
-    STATE = "lesson2"
+    #check for inputs
+    #inputs are reset at the end of functions 
+    if BUTTON1 == 1:
+        STATE = "lesson1"
+        print("lesson1")
+        BUTTON1 = False
+    elif BUTTON2:
+        STATE = "lesson2"
+        BUTTON2 = False
+    elif BUTTON3:
+        STATE = "mainMenu"
+        BUTTON3 = False
 
-    
-
-
-
-    
